d212e70125860da5a104604d3457cffb90621d15
Ayesha rafiq <iamayesharafiq@gmail.com>
2026-08-14 01:11:38 +0500

    added cart controller

---
+++ src/controllers/cart.controller.js
@@ -0,0 +1,73 @@
+import { Cart } from '../models/cart.model.js';
+import { Product } from '../models/product.model.js';
+import { ApiError } from '../utils/apiError.js';
+import { ApiResponse } from '../utils/apiResponse.js';
+import { asyncHandler } from '../utils/asyncHandler.js';
+
+
+const addToCart = asyncHandler( async(req , res) => {
+
+    const { productId , quantity } = req.body;
+    const userId = req.user._id;
+
+    if(!productId || !quantity || quantity < 1){
+        throw new ApiError(400, "Valid product ID and quantity are required");
+    }
+
+    const product = await Product.findById(productId);
+    if(!product){
+        throw new ApiError(400, "Product not found");
+    }
+
+    if(product.stock < quantity) {
+        throw new ApiError(400, `Only ${product.stock} items in stock`);
+    }
+
+    let cart = await Cart.findOne({ owner : userId });
+
+    if(!cart) {
+        cart = Cart.create({
+            owner : userId,
+            items : [{ product : productId , quantity}]
+        })
+    } else {
+        const itemIndex = cart.items.findIndex(
+            (item) => item.product.toString() === productId
+        )
+
+        if(itemIndex > -1) {
+            cart.items[itemIndex].quantity = quantity
+        } else {
+            cart.items.push({ product : productId , quantity });
+        }
+
+        await cart.save();
+    }
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, cart , "Item added to cart successfully"));
+});
+
+const getUserCart = asyncHandler( async( req, res) => {
+    const userId = req.user._id;
+
+    const cart = await Cart.findOne({ owner : userId }).populate({
+        path : 'items.product',
+        select : " name price stock description"
+
+    });
+
+    if(!cart) {
+        return res
+        .status(200)
+        .json(new ApiResponse(200, {items : []}, "Cart is empty"))
+    }
+
+    return res 
+    .status(200)
+    .json(new ApiResponse(200, cart , "Cart fetched successfully"));
+});
+
+
+
