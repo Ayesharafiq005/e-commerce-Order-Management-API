cc0e0e1e181d9b772aa3efd40453c335aca035a5
Ayesha rafiq <iamayesharafiq@gmail.com>
2026-08-21 10:39:44 +0500

    added more verifcations

--- src/routes/category.routes.js
+++ src/routes/category.routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { createCategory } from "../controllers/category.controller.js";
+import { verifyJWT  } from "../middlewares/auth.middleware.js";
+import { verifyAdmin } from "../middlewares/admin.middleware.js";
 
 const router = Router();
 
-router.route("/").post(createCategory);
+router.route("/").post(verifyJWT, verifyAdmin,createCategory);
 
 export default router;
--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import { createProduct , getAllProducts} from "../controllers/product.controller.js";
+import { verifyJWT  } from "../middlewares/auth.middleware.js";
+import { verifyAdmin } from "../middlewares/admin.middleware.js";
+
 
 
 const router = Router();
 
-router.route("/").post(createProduct);
+router.route("/").post(verifyJWT, verifyAdmin, createProduct);
 
 router.route("/").get(getAllProducts);
 
